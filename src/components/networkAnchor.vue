725c079b48b1dc3bf5ba486faecf457911a07bb0
leijinjun <leijinjun@panpay.com.cn>
2019-03-27 18:54:58 +0800

    commit

--- src/components/networkAnchor.vue
+++ src/components/networkAnchor.vue
@@ -1,63 +1,79 @@
 <template>
-	<el-table id="networkAnchor-table"
-    :data="roomList"
-    style="width: 100%"
-    max-height="650"
-    v-loading="tableLoading"
-    :row-class-name="tableRowClassName">
-    <el-table-column
-      prop="roomId"
-      label="房间ID">
-    </el-table-column>
-    <el-table-column label="操作" sortable="true">
-      <template slot-scope="scope">
-        <el-button style="font-size:16px;" v-if="!scope.row.connected"
-          @click.native.prevent="connect(scope.row,scope.$index)"
-          type="text"
-          size="medium">连接
-        </el-button>
-        <el-button style="font-size:16px;" v-else
-          @click.native.prevent="disConnect(scope.row,scope.$index)"
-          type="text"
-          size="medium">断开连接
-        </el-button>
-        <el-button style="font-size:16px;" 
-          @click.native.prevent="goRoomDetail(scope.row.roomId)"
-          type="text"
-          size="medium">查看
-        </el-button>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="连接状态">
-      <template slot-scope="scope">
-        <span v-if="scope.row.connected" style="color: #71C671;">已连接</span>
-        <span v-else style="color: #CD5555;">未连接</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="热度">
-      <template slot-scope="scope">
-        <span><i class="iconfont icon-redu" style="color:#ee5959;"></i>&nbsp;{{scope.row.hn|numberFormatter}}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      prop="roomName"
-      label="房间名称">
-    </el-table-column>
-    <el-table-column
-      prop="nickname"
-      label="主播昵称">
-    </el-table-column>
-    <el-table-column
-      label="房间缩略图">
-      <template slot-scope="scope">
-        <span>
-        	<img :src="scope.row.roomSrc" height="38px;"/>
-        </span>
-      </template>
-    </el-table-column>
-  </el-table>
+	<div>
+		<el-form :inline="true" :model="selectForm" class="demo-form-inline" style="font-size: 26px;">
+		  <el-form-item label="房间Id">
+		    <el-input v-model="selectForm.roomId" placeholder="房间Id"></el-input>
+		  </el-form-item>
+		  <el-form-item label="房间名称">
+		  	<el-input v-model="selectForm.roomName" placeholder="房间名称"></el-input>
+		  </el-form-item>
+		  <el-form-item label="主播昵称">
+		  	<el-input v-model="selectForm.nickname" placeholder="主播昵称"></el-input>
+		  </el-form-item>
+		  <el-form-item>
+		    <el-button type="primary" @click="sumbitSelect">查询</el-button>
+		  </el-form-item>
+		</el-form>
+		<el-table id="networkAnchor-table"
+		    :data="roomList"
+		    style="width: 100%"
+		    max-height="650"
+		    v-loading="tableLoading"
+		    :row-class-name="tableRowClassName">
+		    <el-table-column
+		      prop="roomId"
+		      label="房间ID">
+		    </el-table-column>
+		    <el-table-column label="操作" :sortable="true" :sort-method="sortWithConnected">
+		      <template slot-scope="scope">
+		        <el-button style="font-size:16px;" v-if="!scope.row.connected"
+		          @click.native.prevent="connect(scope.row,scope.$index)"
+		          type="text"
+		          size="medium">连接
+		        </el-button>
+		        <el-button style="font-size:16px;" v-else
+		          @click.native.prevent="disConnect(scope.row,scope.$index)"
+		          type="text"
+		          size="medium">断开连接
+		        </el-button>
+		        <el-button style="font-size:16px;" 
+		          @click.native.prevent="goRoomDetail(scope.row.roomId)"
+		          type="text"
+		          size="medium">查看
+		        </el-button>
+		      </template>
+		    </el-table-column>
+		    <el-table-column
+		      label="连接状态">
+		      <template slot-scope="scope">
+		        <span v-if="scope.row.connected" style="color: #71C671;">已连接</span>
+		        <span v-else style="color: #CD5555;">未连接</span>
+		      </template>
+		    </el-table-column>
+		    <el-table-column
+		      label="热度" :sortable="true" :sort-method="sortWithHn">
+		      <template slot-scope="scope">
+		        <span><i class="iconfont icon-redu" style="color:#ee5959;"></i>&nbsp;{{scope.row.hn|numberFormatter}}</span>
+		      </template>
+		    </el-table-column>
+		    <el-table-column
+		      prop="roomName"
+		      label="房间名称">
+		    </el-table-column>
+		    <el-table-column
+		      prop="nickname"
+		      label="主播昵称">
+		    </el-table-column>
+		    <el-table-column
+		      label="房间缩略图">
+		      <template slot-scope="scope">
+		        <span>
+		        	<img :src="scope.row.roomSrc" height="38px;"/>
+		        </span>
+		      </template>
+		    </el-table-column>
+		</el-table>
+	</div>
 </template>
 
 <script>
@@ -69,6 +85,11 @@ export default {
     return{
 		roomList:[],
 		tableLoading:false,
+		selectForm:{
+			roomId:null,
+			roomName:null,
+			nickname:null,
+		}
 	}
   },
   filters:{
@@ -120,8 +141,16 @@ export default {
 			window.open(`https://opendanmu.com/room/${roomId}`);
 		},
 		sortWithConnected(a,b){
-			return 1;
+			var a1=a.connected?1:0;
+			var b1=b.connected?1:0;
+			return a1-b1;
 		},
+		sortWithHn(a,b){
+			return a.hn-b.hn;
+		},
+		sumbitSelect(){
+			
+		}
 	},
 	mounted () {
 	}
